37c78da67e2a3ee740e19e6a4ceb28c5c7d7afb5
Dmitrii Pososhkov <pososhkovd@yandex.ru>
2025-01-02 12:10:33 +0500

    initial commit

---
+++ src/main/java/org/dmit3ii/limitcontrolmicroservice/LimitControlMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package org.dmit3ii.limitcontrolmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitControlMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimitControlMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/dmit3ii/limitcontrolmicroservice/LimitControlMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dmit3ii.limitcontrolmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitControlMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
