7f6c88993b4fa036648a1f31ac844eb63bb4ff8a
Dmitrii Pososhkov <pososhkovd@yandex.ru>
2025-01-03 11:39:39 +0500

    Добавил настройки БД в application.properties и класс для получения данных с https://openexchangerates.org/

---
+++ src/main/java/org/dmit3ii/limitcontrolmicroservice/model/ExchangeRates.java
@@ -0,0 +1,25 @@
+package org.dmit3ii.limitcontrolmicroservice.model;
+
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
+public class ExchangeRates {
+    private String disclaimer;
+    private String license;
+    private long timestamp;
+    private String base;
+    private Map<String, Double> rates;
+
+    @Override
+    public String toString() {
+        return "ExchangeRates{" +
+                "disclaimer='" + disclaimer + '\'' +
+                ", license='" + license + '\'' +
+                ", timestamp=" + timestamp +
+                ", base='" + base + '\'' +
+                ", rates=" + rates +
+                '}';
+    }
+}
