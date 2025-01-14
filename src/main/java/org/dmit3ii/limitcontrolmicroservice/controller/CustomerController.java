7ec9e501f391801c81b1b6b54345a9b8b61a090b
Dmitrii Pososhkov <pososhkovd@yandex.ru>
2025-01-14 14:43:40 +0500

    добавлен запрос на получение всех установленных лимитов для нужного клиента и категории

--- src/main/java/org/dmit3ii/limitcontrolmicroservice/controller/CustomerController.java
+++ src/main/java/org/dmit3ii/limitcontrolmicroservice/controller/CustomerController.java
@@ -2,15 +2,15 @@ package org.dmit3ii.limitcontrolmicroservice.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.dmit3ii.limitcontrolmicroservice.model.ExpenseCategory;
 import org.dmit3ii.limitcontrolmicroservice.model.Limit;
 import org.dmit3ii.limitcontrolmicroservice.model.LimitDTO;
 import org.dmit3ii.limitcontrolmicroservice.model.mapper.LimitMapper;
 import org.dmit3ii.limitcontrolmicroservice.service.LimitService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -20,10 +20,17 @@ public class CustomerController {
     private final LimitMapper limitMapper;
     private LimitService limitService;
 
-    @PostMapping
+    @PostMapping("/limits")
     public ResponseEntity<Limit> setLimit(@RequestBody LimitDTO limitDTO) {
         Limit newLimit = limitService.setLimit(limitMapper.toLimit(limitDTO));
         log.info("Customer ID={} setting limit {}", newLimit.getAccountTo(), newLimit.getLimitSum());
         return ResponseEntity.ok(newLimit);
+    }
+
+    @GetMapping("/limits/{accountFrom}/{expenseCategory}")
+    public ResponseEntity<List<Limit>> getLimits(@PathVariable("accountFrom") long accountFrom, @PathVariable("expenseCategory") String expenseCategory) {
+        List<Limit> limitList = limitService.getLimits(accountFrom, ExpenseCategory.valueOf(expenseCategory.toUpperCase()));
+       log.info("For customer ID={} getting limit list for category {}", accountFrom, expenseCategory);
+        return ResponseEntity.ok(limitList);
     }
 }
