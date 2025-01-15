518a8207571088fa8d174cab0e3d7d1eda4f59c3
Dmitrii Pososhkov <pososhkovd@yandex.ru>
2025-01-15 17:12:06 +0500

    тестирование подключения к бд в тестах в Jenkins

--- src/test/java/org/dmit3ii/limitcontrolmicroservice/LimitControlMicroserviceApplicationTests.java
+++ src/test/java/org/dmit3ii/limitcontrolmicroservice/LimitControlMicroserviceApplicationTests.java
@@ -2,8 +2,9 @@ package org.dmit3ii.limitcontrolmicroservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-
+@ActiveProfiles("test")
 @SpringBootTest
 class LimitControlMicroserviceApplicationTests {
 
