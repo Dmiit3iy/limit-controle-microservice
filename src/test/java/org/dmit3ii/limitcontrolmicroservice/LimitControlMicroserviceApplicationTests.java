9cc72328fdd9c457d606e419458aa75ac6f32690
Dmitrii Pososhkov <pososhkovd@yandex.ru>
2025-01-15 16:54:06 +0500

    убрал профиль для тестов

--- src/test/java/org/dmit3ii/limitcontrolmicroservice/LimitControlMicroserviceApplicationTests.java
+++ src/test/java/org/dmit3ii/limitcontrolmicroservice/LimitControlMicroserviceApplicationTests.java
@@ -2,9 +2,8 @@ package org.dmit3ii.limitcontrolmicroservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
+
 @SpringBootTest
 class LimitControlMicroserviceApplicationTests {
 
